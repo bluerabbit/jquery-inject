67289470bcd6672e08fa9bc2dbb72cf2966d991e
Akira Kusumoto <akirakusumo10@gmail.com>
2012-05-06 13:35:39 +0900

    initial commit

---
+++ executeScript.js
@@ -0,0 +1,6 @@
+if (window.jQuery == undefined) {
+	var script = document.createElement('script');
+	script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js';
+	script.type = 'text/javascript';
+	document.head.appendChild(script);
+};
